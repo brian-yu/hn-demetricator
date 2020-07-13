45e422bb4f1a38c3a62249fd24b84183ab4c8b50
Chege <d.chege711@gmail.com>
2020-07-12 18:54:11 -0700

    Remove comment counts for posts with zero comments
    Closes #2 . A post that shows "0" draws more attention to itself, 
    which defeats the purpose of demetricating HN

--- hacker_news.js
+++ hacker_news.js
@@ -22,11 +22,8 @@ async function removeUpvotes() {
     // Handle HNES.
     for (const e of document.getElementsByClassName('comments')) {
         if (e instanceof HTMLAnchorElement) {
-            // Don't overwrite posts with 0 comments and posts with new comments.
-            if (e.text != "0" && e.children.length == 0) {
-                e.style.fontSize = ".7rem";
-                e.innerText = "discuss";
-            }
+            e.style.fontSize = ".7rem";
+            e.innerText = "discuss";
         }
     }
 }
